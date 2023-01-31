39c142200e62345848957a030e17be0c1440c45f
“DanMiyakawa” <110312863+DanMiyakawa@users.noreply.github.com>
2023-01-31 00:55:03 +0000

    Chapter04

---
+++ Chapter04/Chapter04.java
@@ -0,0 +1,12 @@
+class Chapter04{
+  public static void main(String[] args){
+    String i = "テスト太郎";
+    System.out.println(i);
+    
+    i = "なんとかなる";
+    System.out.println(i);
+    
+    final int LUCKY_NUMBER = 7;
+    System.out.println(LUCKY_NUMBER);
+  }
+}
