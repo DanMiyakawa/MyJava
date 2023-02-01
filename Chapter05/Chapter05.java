e7c31d821eae55e29501b871ed701c885d371506
“DanMiyakawa” <110312863+DanMiyakawa@users.noreply.github.com>
2023-02-01 04:05:13 +0000

    Chapter0

---
+++ Chapter05/Chapter05.java
@@ -0,0 +1,21 @@
+class Chapter05{
+  public static void main(String[] args){
+    long l = 1334131L;
+    double d = 3.12;
+    boolean b = true;
+   
+    float f = 1.23214f;
+    Integer i = 431;
+    Character c = 'あ';
+   
+    System.out.println(l);
+    System.out.println(d);
+    System.out.println(b);
+    System.out.println(f);
+    System.out.println(i);
+    System.out.println(c);
+
+    int[] array = { 1,2,3,4,5};
+    System.out.println(array[3]);
+  }
+}
